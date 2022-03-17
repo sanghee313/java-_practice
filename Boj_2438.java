6c7ced823d8c7c23f59067f50cebb3f0e025dcd9
DESKTOP-J7FBPBM\82106 <cosmos.csh@gmail.com>
2022-03-17 14:17:32 +0900

    Boj_2439 제출

--- Boj_2438.java
+++ Boj_2438.java
@@ -6,11 +6,16 @@ public class Boj_2438 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int count = sc.nextInt();
+		
 		for (int i = 1; i <= count; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
+			for (int j = 0; j < (count - i); j++) {
+				System.out.print(" ");
 			}
-			System.out.println(" ");
+			
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}			
+			System.out.print("\n");
 		}
 		sc.close();
 	}
--- Boj_2439.java
+++ Boj_2439.java
@@ -1,5 +1,15 @@
 package testPjt;
 
-public class Boj_2439 {
+import java.util.Scanner;
 
+public class Boj_2439 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int count = in.nextInt();
+		for (int i = 0; i < count; i++) {
+			for (int j = 0; j < args.length; j++) {
+				
+			}
+		}
+	}
 }
